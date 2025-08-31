216e66c0670155a32f44c0d3f10fb3291992debb
Jillo Woche <jillo.abdulla@gmail.com>
2025-09-01 02:56:14 +0300

    fix: restore component exports and update title

---
+++ src/components/index.ts
@@ -0,0 +1,18 @@
+// Component exports
+export { default as AuthorChip } from "./AuthorChip";
+export { default as DatePicker } from "./DatePicker";
+export { default as InfoBox } from "./InfoBox";
+export { default as InfoSection } from "./InfoSection";
+export { default as MetadataPill } from "./MetadataPill";
+export { default as MyProposalCard } from "./MyProposalCard";
+export { default as Navbar } from "./Navbar";
+export { default as Pagination } from "./Pagination";
+export { default as ProposalCard } from "./ProposalCard";
+export { default as ProposalContent } from "./ProposalContent";
+export { default as ProposalsList } from "./ProposalsList";
+export { default as RelativeTime } from "./RelativeTime";
+export { default as SearchSection } from "./SearchSection";
+export { default as StatusPill } from "./StatusPill";
+export { default as TimePicker } from "./TimePicker";
+export { default as VotingCard } from "./VotingCard";
+export { default as VotingStats } from "./VotingStats";
